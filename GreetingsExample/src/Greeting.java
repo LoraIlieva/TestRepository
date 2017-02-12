8cc627cf957cd1c0b3d6b6bebcc3aae97fe8732e
student <student@uni>
2017-02-12 19:09:11 +0200

    greetSpanish added; hello --> hi

--- GreetingsExample/src/Greeting.java
+++ GreetingsExample/src/Greeting.java
@@ -7,10 +7,14 @@ public class Greeting {
 		gr.greetItalian();
 	}
 	public void greetEnglish(){
-		System.out.println("Hello");
+		System.out.println("Hi");
 	}
 	public void greetItalian()
 	{
 		System.out.println("Ciao");
 	}	
+	public void greetSpanish()
+	{
+		System.out.println("Hola");
+	}
 }
